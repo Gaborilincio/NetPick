02e08ee7f64b549d8edb4c26f4dfec0c05b6bdd1
Gaborilincio <ga.mendez@duocuc.cl>
2025-11-24 21:22:55 -0300

    cartprovider agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // Importas tu proveedor
+import { AuthProvider } from './context/AuthContext'; 
 import Navbar from './components/organisms/Navbar';
 import Footer from './components/organisms/Footer';
 import Home from './pages/Home';
@@ -14,11 +14,13 @@ import EditProfile from './pages/EditProfile';
 import Cart from './pages/Cart';
 import ForgotPassword from './pages/ForgotPassword';
 import './styles/global.css';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
     <Router>
       <AuthProvider>
+        <CartProvider>
         <Navbar />
         <main className="flex-grow-1">
           <Routes>
@@ -35,6 +37,7 @@ function App() {
           </Routes>
         </main>
         <Footer />
+        </CartProvider>
       </AuthProvider>
     </Router>
   );
--- src/components/atoms/FormGroup.jsx
+++ src/components/atoms/FormGroup.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
+  import React from 'react';
+  import { Form } from 'react-bootstrap';
 
-function FormGroup({ label, children, ...props }) {
-  return (
-    <Form.Group {...props}>
-      {label && <Form.Label>{label}</Form.Label>}
-      {children}
-    </Form.Group>
-  );
-}
+  function FormGroup({ label, children, ...props }) {
+    return (
+      <Form.Group {...props}>
+        {label && <Form.Label>{label}</Form.Label>}
+        {children}
+      </Form.Group>
+    );
+  }
 
-export default FormGroup;
+  export default FormGroup;
