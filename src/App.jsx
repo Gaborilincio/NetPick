0308cb6f8925da7a35723fbb0ca7ebd041a9f55d
Gaborilincio <ga.mendez@duocuc.cl>
2025-11-24 21:38:15 -0300

    alo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; 
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+
 import Navbar from './components/organisms/Navbar';
 import Footer from './components/organisms/Footer';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -13,30 +16,36 @@ import Profile from './pages/Profile';
 import EditProfile from './pages/EditProfile';
 import Cart from './pages/Cart';
 import ForgotPassword from './pages/ForgotPassword';
+import Account from './pages/Account'; 
+
 import './styles/global.css';
-import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <CartProvider>
-        <Navbar />
-        <main className="flex-grow-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/category/:categoryName" element={<Category />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/edit-profile" element={<EditProfile />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-          </Routes>
-        </main>
-        <Footer />
+
+          <Navbar />
+
+          <main className="flex-grow-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/productos" element={<Products />} />
+              <Route path="/productos/:id" element={<ProductDetail />} />
+              <Route path="/categorias" element={<Category />} />
+              <Route path="/carrito" element={<Cart />} />
+              <Route path="/perfil" element={<Profile />} />
+              <Route path="/perfil/editar" element={<EditProfile />} />
+              <Route path="/recuperar" element={<ForgotPassword />} />
+              <Route path="/cuenta" element={<Account />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </main>
+
+          <Footer />
+
         </CartProvider>
       </AuthProvider>
     </Router>
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; 
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css';
 import './styles/Home.css';
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <AuthProvider> 
-        <App />
-      </AuthProvider> 
+    <App />
   </StrictMode>,
-)
+);
