0f7e2c14ff43b4936aeb4490fb41e8de01c5b424
Gaborilincio <ga.mendez@duocuc.cl>
2025-12-10 18:08:39 -0300

    AMEN 1

--- src/components/organisms/PaymentForm.jsx
+++ src/components/organisms/PaymentForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { PurchaseService } from '../services/PurchaseService'; 
+import { PurchaseService } from '../../services/PurchaseService'; 
 
 const PaymentForm = ({ onSubmit, loading }) => {
     const [metodosPago, setMetodosPago] = useState([]);
